4ae911c9d5dfe16fbc2e3ef96c72fdbc3fe12d62
Kairo Araujo <kdearaujo@vmware.com>
2023-04-25 11:07:00 +0200

    RSTUF_WORKER_ID as optional container parameter (#277)
    * RSTUF_WORKER_ID as optional container parameter
    
    This commit documents and move `RSTUF_WORKER_ID` environment variable as
    optional.
    
    The `RSTUF_WORKER_ID` is now optional and by default uses the container
    hostname.
    
    This enables RSTUF user spin multiple replicas using for example
    kubernetes without dealing to generate multiple environment variables.
    
    Closes #260
    
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>
    
    * Add the RSTUF_WORKER_ID in tox setenv
    
    It is required once the tests doesn't use the entrypoint to start the
    service.
    
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>
    
    ---------
    
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>

--- entrypoint-dev.sh
+++ entrypoint-dev.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+if [ -z $RSTUF_WORKER_ID ]; then
+    export RSTUF_WORKER_ID=$(hostname)
+fi
 alembic upgrade head
 watchmedo auto-restart -d /opt/repository-service-tuf-worker -R -p '*.py' -- supervisord -c supervisor-dev.conf
