f91cf78ff7e9d1c03f0b35e09b247e95ee228b39
Kairo Araujo <kdearaujo@vmware.com>
2023-09-21 10:51:25 +0300

    RSTUF Interface refactoring (IServices) (#389)
    * RSTUF Interface refactoring (IServices)
    
    - The old `repository.refresh_settings` handling the IServices
      configuration was moved to the `interfaces`
    - Now the way to importa a `I{Services}` uses directly the dynaconf
    - Adds better error mensages for wrong configuration
    - Support a service has one setting with multiple environment variables
      what is helpful for third-part libraries that uses environment vars
    
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>
    
    * Fix comments and tests for interfaces
    
    - Improve the comments
    - Implement a better test for the all settings None
    - Add asserts related to the stub
    
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>
    
    ---------
    
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>

---
+++ tests/unit/tuf_repository_service_worker/test_interfaces.py
@@ -0,0 +1,171 @@
+import pretend
+import pytest
+
+from repository_service_tuf_worker import interfaces
+
+
+class TestInterfaces:
+    def test_IKeyVault(self):
+        class TestKeyVault(interfaces.IKeyVault):
+            @classmethod
+            def configure():
+                ...
+
+            def get():
+                ...
+
+            @classmethod
+            def settings():
+                ...
+
+        test_keyvault = TestKeyVault()
+
+        assert isinstance(test_keyvault, interfaces.IKeyVault)
+
+    def test_IStorage(self):
+        class TestStorage(interfaces.IStorage):
+            @classmethod
+            def configure():
+                ...
+
+            @classmethod
+            def settings():
+                ...
+
+            def get():
+                ...
+
+            def put():
+                ...
+
+        test_keyvault = TestStorage()
+
+        assert isinstance(test_keyvault, interfaces.IStorage)
+
+    def test__setup_service_dynaconf_invalid_interface(self, monkeypatch):
+        test_cls = pretend.stub(
+            __subclasses__=pretend.call_recorder(
+                lambda: [pretend.stub(__name__="FakeService")]
+            ),
+            __name__="IFake",
+        )
+        test_settings = interfaces.Dynaconf()
+        test_settings.RSTUF_FAKE_BACKEND = "FAKESERVICE"
+        test_settings.FAKE_VAR1 = "value1"
+        test_settings.FAKE_VAR2 = "value2"
+        test_backend = pretend.stub(
+            settings=pretend.call_recorder(
+                lambda: [
+                    interfaces.ServiceSettings(
+                        names=["FAKE_VAR1", "FAKE_VAR2"],
+                        argument="var",
+                        required=True,
+                    )
+                ]
+            ),
+            configure=pretend.call_recorder(lambda *a: None),
+        )
+        test_module = pretend.stub(FAKESERVICE=test_backend)
+
+        monkeypatch.setattr(
+            "repository_service_tuf_worker.interfaces.importlib.import_module",
+            lambda *a: test_module,
+        )
+
+        with pytest.raises(ValueError) as err:
+            interfaces._setup_service_dynaconf(
+                test_cls, test_settings.RSTUF_FAKE_BACKEND, test_settings
+            )
+
+        assert "Invalid Interface IFake" in str(err)
+        assert test_backend.settings.calls == [pretend.call(), pretend.call()]
+        assert test_backend.configure.calls == []
+        assert test_cls.__subclasses__.calls == [pretend.call()]
+
+    def test__setup_service_dynaconf_all_settings_none(self, monkeypatch):
+        class FakeStorage(interfaces.IStorage):
+            def __init__(self, var1: str, var2: str):
+                self._var1 = var1
+                self._var2 = var2
+
+            @classmethod
+            def configure(cls, settings):
+                ...
+
+            @classmethod
+            def settings(cls):
+                return [
+                    interfaces.ServiceSettings(
+                        names=["TEST_STORAGE_VAR1", "TEST_STORAGE_VARIABLE1"],
+                        argument="var1",
+                        required=False,
+                    ),
+                    interfaces.ServiceSettings(
+                        names=["TEST_STORAGE_VAR2"],
+                        argument="var2",
+                        required=False,
+                    ),
+                ]
+
+            def get():
+                ...
+
+            def put():
+                ...
+
+        test_settings = interfaces.Dynaconf()
+        test_settings.STORAGE_BACKEND = "FAKESTORAGE"
+        test_settings.TEST_STORAGE_VAR1 = None
+        test_settings.TEST_STORAGE_VAR2 = None
+
+        monkeypatch.setattr(
+            "repository_service_tuf_worker.interfaces.importlib.import_module",
+            lambda *a: test_module,
+        )
+        test_module = pretend.stub(FAKESTORAGE=FakeStorage)
+
+        result = interfaces.IStorage.from_dynaconf(test_settings)
+        assert result is None
+        assert isinstance(test_settings.STORAGE, interfaces.IStorage)
+        assert test_settings.STORAGE._var1 is None
+        assert test_settings.STORAGE._var2 is None
+
+    def test__setup_service_dynaconf_missing_config(self, monkeypatch):
+        test_cls = pretend.stub(
+            __subclasses__=pretend.call_recorder(
+                lambda: [pretend.stub(__name__="FakeService")]
+            ),
+            __name__="IFake",
+        )
+        test_settings = interfaces.Dynaconf()
+        test_settings.RSTUF_FAKE_BACKEND = "FAKESERVICE"
+        test_backend = pretend.stub(
+            FAKESERVICE=pretend.stub(
+                settings=pretend.call_recorder(
+                    lambda: [
+                        interfaces.ServiceSettings(
+                            names=["FAKE_VAR1", "FAKE_VAR2"],
+                            argument="var",
+                            required=True,
+                        )
+                    ]
+                ),
+            )
+        )
+
+        monkeypatch.setattr(
+            "repository_service_tuf_worker.interfaces.importlib.import_module",
+            lambda *a: test_backend,
+        )
+
+        with pytest.raises(AttributeError) as err:
+            interfaces._setup_service_dynaconf(
+                test_cls, test_settings.RSTUF_FAKE_BACKEND, test_settings
+            )
+
+        assert (
+            "'Settings' object has no attribute(s) (environment variables): "
+            "RSTUF_FAKE_VAR1 or RSTUF_FAKE_VAR2"
+        ) in str(err)
+        assert test_backend.FAKESERVICE.settings.calls == [pretend.call()]
+        assert test_cls.__subclasses__.calls == [pretend.call()]
