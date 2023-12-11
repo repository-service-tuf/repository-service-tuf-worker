2646e53977b1dfb69a138106017b69b52ee2580e
Lukas Puehringer <lukas.puehringer@nyu.edu>
2023-12-11 16:27:35 +0100

    Add custom FileSigner to load key from relative path
    The base path is provided via envrionment variable.
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- repository_service_tuf_worker/_signer.py
+++ repository_service_tuf_worker/_signer.py
@@ -1,4 +1,49 @@
-from securesystemslib.signer import Key, Signer
+import os
+from pathlib import Path
+from typing import Optional
+
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from securesystemslib.signer import (
+    SIGNER_FOR_URI_SCHEME,
+    CryptoSigner,
+    Key,
+    SecretsHandler,
+    Signer,
+)
+
+
+class FileSigner(CryptoSigner):
+    """File-based signer implementation.
+
+    Overrides `CryptoSigner.from_priv_key_uri` to load private key file from a
+
+    * directory, defined in envvar: `RSTUF_ONLINE_KEY_DIR`, and a
+    * file name, defined in the passed uri: `fn:<file name>`
+
+    """
+
+    SCHEME = "fn"
+    DIR_VAR = "RSTUF_ONLINE_KEY_DIR"
+
+    @classmethod
+    def from_priv_key_uri(
+        cls,
+        priv_key_uri: str,
+        public_key: Key,
+        secrets_handler: Optional[SecretsHandler] = None,
+    ) -> "FileSigner":
+        _, _, file_name = priv_key_uri.partition(":")
+        dir_ = os.environ[cls.DIR_VAR]
+
+        with open(Path(dir_, file_name), "rb") as f:
+            private_pem = f.read()
+
+        private_key = load_pem_private_key(private_pem, None)
+        return FileSigner(private_key, public_key)
+
+
+# Register signer for scheme for usage via generic `Signer.from_priv_key_uri`
+SIGNER_FOR_URI_SCHEME.update({FileSigner.SCHEME: FileSigner})
 
 
 class SignerStore:
