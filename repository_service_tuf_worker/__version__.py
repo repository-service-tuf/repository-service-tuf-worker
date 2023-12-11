a3f4553bee8e588823b51328d9a6b454746397b7
Kairo Araujo <kairo.araujo@testifysec.com>
2023-12-11 12:43:27 +0100

    Prepare Release v0.10.0b1 (#428)
    * Prepare Release v0.10.0b1
    
    - Update the Copyright as project is now part of OpenSSF
    - Make the setup.py version dynamic. (Note: we don't release it as a
      python package, but we use it in tox. In the future it could be used
      as well)
    - Bump release to v0.10.0b1
    
    Signed-off-by: Kairo de Araujo <kairo.araujo@testifysec.com>
    
    * Revert change in setup.py which makes tests fail
    
    We can do this change in another opportunity
    
    Signed-off-by: Kairo de Araujo <kairo.araujo@testifysec.com>
    
    ---------
    
    Signed-off-by: Kairo de Araujo <kairo.araujo@testifysec.com>

--- repository_service_tuf_worker/__version__.py
+++ repository_service_tuf_worker/__version__.py
@@ -1,7 +1,8 @@
+# SPDX-FileCopyrightText: 2023 Repository Service for TUF Contributors
 # SPDX-FileCopyrightText: 2022-2023 VMware Inc
 #
 # SPDX-License-Identifier: MIT
 
-version = "0.9.0b1"
-copyright = "Copyright (c) 2022-2023 VMware Inc"
+version = "0.10.0b1"
+copyright = "Copyright (c) 2023 Repository Service for TUF Contributors"
 author = "Kairo de Araujo"
--- repository_service_tuf_worker/models/targets/__init__.py
+++ repository_service_tuf_worker/models/targets/__init__.py
@@ -1,3 +1,4 @@
+# SPDX-FileCopyrightText: 2023 Repository Service for TUF Contributors
 # SPDX-FileCopyrightText: 2022-2023 VMware Inc
 #
 # SPDX-License-Identifier: MIT
