d5d915c1a08af380ff8a5088a59d80666bbad4a9
Lukas Puehringer <lukas.puehringer@nyu.edu>
2024-02-05 19:12:05 +0100

    Add priv key uri support to SignerStore
    implements option 2 from repository-service-tuf/repository-service-tuf#580
    supersedes #427 (does not include a custom "relative file path signer",
    can be added in a follow-up PR)
    
    --
    
    Change `SignerStore.get` to load non-cached signers from private key uri
    configured on the passed public key in a  "x-rstuf-online-key-uri" field.
    
    If the public key does not include a uri, RSTUF_KEYVAULT_BACKEND is used
    as fallback.
    
    securesystemslib.signer.CryptoSigner is "registered" to load signers
    from private key files. No key specific secrets handling is added. This
    means the keys must be stored unencryped, preferrably using the secrets
    handling of the deployment platform (e.g. docker secrets).
    
    Default schemes in `securesystemslib.signer.SIGNER_FOR_URI_SCHEME` can
    be used but are untested.
    
    **Tests**
    Add test to load actual signer from private key file.
    
    Uses new unencrypted ed25519 private key copied from:
    secure-systems-lab/securesystemslib@7952c3f
    
    Public key stubs in other tests are updated, because signer store now
    reads the `unrecognized_fields` attribute, which is mandatory in Key
    objects.
    
    --
    
    implements option 2 described in
    https://github.com/repository-service-tuf/repository-service-tuf/issues/580
    
    mostly supersedes #427 (does not include a custom "relative file path
    signer")
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- repository_service_tuf_worker/signer.py
+++ repository_service_tuf_worker/signer.py
@@ -3,9 +3,20 @@
 # SPDX-License-Identifier: MIT
 
 from dynaconf import Dynaconf
-from securesystemslib.signer import Key, Signer
+from securesystemslib.signer import (
+    SIGNER_FOR_URI_SCHEME,
+    CryptoSigner,
+    Key,
+    Signer,
+)
 
 from repository_service_tuf_worker.interfaces import IKeyVault
+
+RSTUF_ONLINE_KEY_URI_FIELD = "x-rstuf-online-key-uri"
+
+# Register non-default securesystemslib file signer
+# secure-systems-lab/securesystemslib#617
+SIGNER_FOR_URI_SCHEME[CryptoSigner.FILE_URI_SCHEME] = CryptoSigner
 
 
 class SignerStore:
@@ -16,15 +27,25 @@ class SignerStore:
         self._signers: dict[str, Signer] = {}
 
     def get(self, key: Key) -> Signer:
-        """Return signer for passed key."""
+        """Return signer for passed key.
+
+        - signer is loaded from the uri included in the passed public key
+          (see SIGNER_FOR_URI_SCHEME for available uri schemes)
+        - RSTUF_KEYVAULT_BACKEND is used as fallback, if no URI is included
+
+        """
 
         if key.keyid not in self._signers:
-            vault = self._settings.get("KEYVAULT")
-            if not isinstance(vault, IKeyVault):
-                raise ValueError(
-                    "RSTUF_KEYVAULT_BACKEND is required for online signing"
-                )
+            if uri := key.unrecognized_fields.get(RSTUF_ONLINE_KEY_URI_FIELD):
+                self._signers[key.keyid] = Signer.from_priv_key_uri(uri, key)
 
-            self._signers[key.keyid] = vault.get(key)
+            else:
+                vault = self._settings.get("KEYVAULT")
+                if not isinstance(vault, IKeyVault):
+                    raise ValueError(
+                        "RSTUF_KEYVAULT_BACKEND is required for online signing"
+                    )
+
+                self._signers[key.keyid] = vault.get(key)
 
         return self._signers[key.keyid]
