b70143c0a5a8c8b5a647c73bfd1129684297998c
Lukas Pühringer <lukas.puehringer@nyu.edu>
2024-02-07 18:32:23 +0100

    Add priv key uri support to SignerStore (#451)
    implements option 2 from repository-service-tuf/repository-service-tuf#580
    supersedes #427 (does not include a custom "relative file path signer",
    can be added in a follow-up PR)
    
    --
    
    Change `SignerStore.get` to load non-cached signers from private key uri
    configured on the passed public key in a  "x-rstuf-online-key-uri" field.
    
    If the public key does not include a uri, RSTUF_KEYVAULT_BACKEND is used
    as fallback.
    
    securesystemslib.signer.CryptoSigner is "registered" to load signers
    from private key files. No key specific secrets handling is added. This
    means the keys must be stored unencryped, preferrably using the secrets
    handling of the deployment platform (e.g. docker secrets).
    
    Default schemes in `securesystemslib.signer.SIGNER_FOR_URI_SCHEME` can
    be used but are untested.
    
    **Tests**
    Add test to load actual signer from private key file.
    
    Uses new unencrypted ed25519 private key copied from:
    secure-systems-lab/securesystemslib@7952c3f
    
    Public key stubs in other tests are updated, because signer store now
    reads the `unrecognized_fields` attribute, which is mandatory in Key
    objects.
    
    --
    
    implements option 2 described in
    https://github.com/repository-service-tuf/repository-service-tuf/issues/580
    
    mostly supersedes #427 (does not include a custom "relative file path
    signer")
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- tests/unit/tuf_repository_service_worker/test_signer.py
+++ tests/unit/tuf_repository_service_worker/test_signer.py
@@ -2,11 +2,19 @@
 #
 # SPDX-License-Identifier: MIT
 
+from pathlib import Path
+
 import pytest
 from pretend import stub
+from securesystemslib.signer import CryptoSigner, Key
 
 from repository_service_tuf_worker.interfaces import IKeyVault
-from repository_service_tuf_worker.signer import SignerStore
+from repository_service_tuf_worker.signer import (
+    RSTUF_ONLINE_KEY_URI_FIELD,
+    SignerStore,
+)
+
+_FILES = Path(__file__).parent.parent.parent / "files"
 
 
 class TestSigner:
@@ -36,7 +44,7 @@ class TestSigner:
 
         fake_id = "fake_id"
         fake_signer = stub()
-        fake_key = stub(keyid=fake_id)
+        fake_key = stub(keyid=fake_id, unrecognized_fields={})
         fake_settings = stub(get=lambda x: FakeKeyVault())
 
         store = SignerStore(fake_settings)
@@ -47,10 +55,35 @@ class TestSigner:
 
     def test_get_no_vault(self):
         fake_id = "fake_id"
-        fake_key = stub(keyid=fake_id)
+        fake_key = stub(keyid=fake_id, unrecognized_fields={})
         fake_settings = stub(get=lambda x: None)
 
         store = SignerStore(fake_settings)
 
         with pytest.raises(ValueError):
             store.get(fake_key)
+
+    def test_get_from_file_uri(self):
+        path = _FILES / "pem" / "ed25519_private.pem"
+        uri = f"file:{path}?encrypted=false"
+
+        key_metadata = {
+            "keytype": "ed25519",
+            "scheme": "ed25519",
+            "keyval": {
+                "public": (
+                    "4f66dabebcf30628963786001984c0b7"
+                    "5c175cdcf3bc4855933a2628f0cd0a0f"
+                )
+            },
+            RSTUF_ONLINE_KEY_URI_FIELD: uri,
+        }
+
+        fake_id = "fake_id"
+        key = Key.from_dict(fake_id, key_metadata)
+
+        fake_settings = stub()
+        store = SignerStore(fake_settings)
+        signer = store.get(key)
+
+        assert isinstance(signer, CryptoSigner)
