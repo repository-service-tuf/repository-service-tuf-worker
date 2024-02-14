c0e318dc508f48bed46c49aee5770ad268613771
Lukas Pühringer <lukas.puehringer@nyu.edu>
2024-02-14 13:06:01 +0100

    Add FileNameSigner and register in SignerStore (#455)
    * Create and use public key fixture in test_signer
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>
    
    * Add FileNameSigner and register in SignerStore
    
    The custom FileNameSigner is similar to the previously registered
    CryptoSigner provided by securesystemslib, with the following
    differences:
    
    - uri format is `fn:<file name>`
    - a base path for the key in <file name> must be provided via
      environment variable RSTUF_ONLINE_KEY_DIR
    - only **unencrypted** keys are supported
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>
    
    * Remove SignerStore._settings attribute
    
    This was only needed for the KEYVAULT settings. We can cache that
    directly.
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>
    
    * Parse ambient signer setting into isolaged env
    
    Update SignerStore to cache known ambient signer settings
    (currently only ONLINE_KEY_DIR) and provide it in an isolated
    environment under the expected environment variable name.
    
    see https://github.com/repository-service-tuf/repository-service-tuf-worker/pull/455#issuecomment-1940942231
    for context
    
    **Related changes**
    - add `isolated_env` context manager
    - update tests to use Dynaconf instead of stub
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>
    
    * Document `RSTUF_ONLINE_KEY_DIR` in Docker_README.md
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>
    
    * Fix spelling
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>
    
    * Remove TUF metadata details in Docker_README
    
    Co-authored-by: Kairo Araujo <kairo@kairo.eti.br>
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>
    
    ---------
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>
    Co-authored-by: Kairo Araujo <kairo@kairo.eti.br>

--- repository_service_tuf_worker/signer.py
+++ repository_service_tuf_worker/signer.py
@@ -1,29 +1,114 @@
 # SPDX-FileCopyrightText: 2024 Repository Service for TUF Contributors
 #
 # SPDX-License-Identifier: MIT
+import os
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Optional
 
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from dynaconf import Dynaconf
 from securesystemslib.signer import (
     SIGNER_FOR_URI_SCHEME,
     CryptoSigner,
     Key,
+    SecretsHandler,
     Signer,
 )
 
 from repository_service_tuf_worker.interfaces import IKeyVault
+
+
+class FileNameSigner(CryptoSigner):
+    """File-based signer implementation.
+
+    Provide method to load **unencrypted** PKCS8/PEM private key from file.
+
+    File path is constructed by joining base path in environment variable
+    ``RSTUF_ONLINE_KEY_DIR`` with file in ``priv_key_uri``.
+
+    NOTE: Make sure to use the secrets management service of your deployment
+    platform to protect your private key!
+
+    Example::
+
+        RSTUF_ONLINE_KEY_DIR (env) "/run/secrets"
+        priv_key_uri (arg): "fn:foo"
+
+        File path: "/run/secrets/foo"
+
+    Raises:
+        KeyError: RSTUF_ONLINE_KEY_DIR environment variable not set
+        OSError: file cannot be loaded
+        ValueError: uri has no file name, or private key cannot be decoded,
+                or type does not match public key
+        cryptography.exceptions.UnsupportedAlgorithm: key type not supported
+
+    """
+
+    SCHEME = "fn"
+    DIR_VAR = "RSTUF_ONLINE_KEY_DIR"
+
+    @classmethod
+    def from_priv_key_uri(
+        cls,
+        priv_key_uri: str,
+        public_key: Key,
+        secrets_handler: Optional[SecretsHandler] = None,
+    ) -> "FileNameSigner":
+        _, _, file_name = priv_key_uri.partition(":")
+        if not file_name:
+            raise ValueError(
+                f"bad uri: expected '{cls.SCHEME}:<file name>', "
+                f"'got {priv_key_uri}'"
+            )
+
+        dir_ = os.environ[cls.DIR_VAR]
+
+        with open(Path(dir_, file_name), "rb") as f:
+            private_pem = f.read()
+
+        private_key = load_pem_private_key(private_pem, None)
+        return cls(private_key, public_key)
+
 
 RSTUF_ONLINE_KEY_URI_FIELD = "x-rstuf-online-key-uri"
 
 # Register non-default securesystemslib file signer
 # secure-systems-lab/securesystemslib#617
 SIGNER_FOR_URI_SCHEME[CryptoSigner.FILE_URI_SCHEME] = CryptoSigner
+# Register custom FileNameSigner
+SIGNER_FOR_URI_SCHEME[FileNameSigner.SCHEME] = FileNameSigner
+
+
+@contextmanager
+def isolated_env(env: dict[str, str]):
+    """Temporarily replace environment."""
+    orig_env = dict(os.environ)
+    os.environ.clear()
+    os.environ.update(env)
+    try:
+        yield
+    finally:
+        os.environ.clear()
+        os.environ.update(orig_env)
 
 
 class SignerStore:
-    """Generic signer store."""
+    """Generic signer store.
+
+    Parses ambient signer settings and provides them in an isolated environment
+    under the expected names.
+    """
 
     def __init__(self, settings: Dynaconf):
-        self._settings = settings
+        # Cache known ambient settings
+        self._ambient_settings: dict[str, str] = {}
+        if key_dir := settings.get("ONLINE_KEY_DIR"):
+            self._ambient_settings[FileNameSigner.DIR_VAR] = key_dir
+
+        # Cache KEYVAULT setting as fallback
+        self._vault = settings.get("KEYVAULT")
         self._signers: dict[str, Signer] = {}
 
     def get(self, key: Key) -> Signer:
@@ -31,21 +116,25 @@ class SignerStore:
 
         - signer is loaded from the uri included in the passed public key
           (see SIGNER_FOR_URI_SCHEME for available uri schemes)
+        - additional signer settings can be provided "ambiently" (see __init__)
         - RSTUF_KEYVAULT_BACKEND is used as fallback, if no URI is included
 
         """
 
         if key.keyid not in self._signers:
             if uri := key.unrecognized_fields.get(RSTUF_ONLINE_KEY_URI_FIELD):
-                self._signers[key.keyid] = Signer.from_priv_key_uri(uri, key)
+                # (Re-)export ambient settings in isolated environment
+                with isolated_env(self._ambient_settings):
+                    signer = Signer.from_priv_key_uri(uri, key)
+
+                self._signers[key.keyid] = signer
 
             else:
-                vault = self._settings.get("KEYVAULT")
-                if not isinstance(vault, IKeyVault):
+                if not isinstance(self._vault, IKeyVault):
                     raise ValueError(
                         "RSTUF_KEYVAULT_BACKEND is required for online signing"
                     )
 
-                self._signers[key.keyid] = vault.get(key)
+                self._signers[key.keyid] = self._vault.get(key)
 
         return self._signers[key.keyid]
--- tests/unit/tuf_repository_service_worker/test_signer.py
+++ tests/unit/tuf_repository_service_worker/test_signer.py
@@ -3,18 +3,35 @@
 # SPDX-License-Identifier: MIT
 
 from pathlib import Path
+from unittest.mock import patch
 
 import pytest
+from dynaconf import Dynaconf
 from pretend import stub
 from securesystemslib.signer import CryptoSigner, Key
 
 from repository_service_tuf_worker.interfaces import IKeyVault
 from repository_service_tuf_worker.signer import (
     RSTUF_ONLINE_KEY_URI_FIELD,
+    FileNameSigner,
     SignerStore,
 )
 
 _FILES = Path(__file__).parent.parent.parent / "files"
+
+
+@pytest.fixture()
+def key_metadata():
+    return {
+        "keytype": "ed25519",
+        "scheme": "ed25519",
+        "keyval": {
+            "public": (
+                "4f66dabebcf30628963786001984c0b7"
+                "5c175cdcf3bc4855933a2628f0cd0a0f"
+            )
+        },
+    }
 
 
 class TestSigner:
@@ -22,9 +39,9 @@ class TestSigner:
         fake_id = "fake_id"
         fake_signer = stub()
         fake_key = stub(keyid=fake_id)
-        fake_settings = stub()
 
-        store = SignerStore(fake_settings)
+        settings = Dynaconf()
+        store = SignerStore(settings)
         store._signers[fake_id] = fake_signer
 
         assert store.get(fake_key) == fake_signer
@@ -45,9 +62,9 @@ class TestSigner:
         fake_id = "fake_id"
         fake_signer = stub()
         fake_key = stub(keyid=fake_id, unrecognized_fields={})
-        fake_settings = stub(get=lambda x: FakeKeyVault())
 
-        store = SignerStore(fake_settings)
+        settings = Dynaconf(KEYVAULT=FakeKeyVault())
+        store = SignerStore(settings)
 
         assert not store._signers
         assert store.get(fake_key) == fake_signer
@@ -56,34 +73,61 @@ class TestSigner:
     def test_get_no_vault(self):
         fake_id = "fake_id"
         fake_key = stub(keyid=fake_id, unrecognized_fields={})
-        fake_settings = stub(get=lambda x: None)
 
-        store = SignerStore(fake_settings)
+        settings = Dynaconf()
+        store = SignerStore(settings)
 
         with pytest.raises(ValueError):
             store.get(fake_key)
 
-    def test_get_from_file_uri(self):
+    def test_get_from_file_uri(self, key_metadata):
         path = _FILES / "pem" / "ed25519_private.pem"
         uri = f"file:{path}?encrypted=false"
-
-        key_metadata = {
-            "keytype": "ed25519",
-            "scheme": "ed25519",
-            "keyval": {
-                "public": (
-                    "4f66dabebcf30628963786001984c0b7"
-                    "5c175cdcf3bc4855933a2628f0cd0a0f"
-                )
-            },
-            RSTUF_ONLINE_KEY_URI_FIELD: uri,
-        }
+        key_metadata[RSTUF_ONLINE_KEY_URI_FIELD] = uri
 
         fake_id = "fake_id"
         key = Key.from_dict(fake_id, key_metadata)
 
-        fake_settings = stub()
-        store = SignerStore(fake_settings)
+        settings = Dynaconf()
+        store = SignerStore(settings)
         signer = store.get(key)
 
         assert isinstance(signer, CryptoSigner)
+
+    def test_get_from_file_name_uri(self, key_metadata):
+        dir_ = _FILES / "pem"
+        uri = "fn:ed25519_private.pem"
+
+        key_metadata[RSTUF_ONLINE_KEY_URI_FIELD] = uri
+        fake_id = "fake_id"
+        key = Key.from_dict(fake_id, key_metadata)
+
+        settings = Dynaconf(ONLINE_KEY_DIR=str(dir_))
+        store = SignerStore(settings)
+        signer = store.get(key)
+
+        assert isinstance(signer, FileNameSigner)
+
+    def test_get_from_file_name_uri_no_filename(self):
+        uri = "fn:"
+        settings = Dynaconf()
+        store = SignerStore(settings)
+        fake_key = stub(
+            keyid="fake_id",
+            unrecognized_fields={RSTUF_ONLINE_KEY_URI_FIELD: uri},
+        )
+
+        with pytest.raises(ValueError):
+            store.get(fake_key)
+
+    def test_get_from_file_name_uri_no_envvar(self):
+        uri = "fn:foo.pem"
+        settings = Dynaconf()
+        store = SignerStore(settings)
+        fake_key = stub(
+            keyid="fake_id",
+            unrecognized_fields={RSTUF_ONLINE_KEY_URI_FIELD: uri},
+        )
+
+        with patch.dict("os.environ", {}, clear=True), pytest.raises(KeyError):
+            store.get(fake_key)
